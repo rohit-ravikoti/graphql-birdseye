c8a9cd66b56065da1c6f925688d97ab002560c25
Alexey Rodionov <fluorescent.hallucinogen@gmail.com>
2019-02-24 13:25:44 +0300

    Improve performance

--- src/3d-graph/index.ts
+++ src/3d-graph/index.ts
@@ -12,7 +12,7 @@ export default class Graph3D {
             (el)
             .graphData(gData)
             .nodeAutoColorBy('id')
-            .nodeResolution(128)
+            .nodeResolution(32)
             .linkDirectionalParticles(3)
             .linkDirectionalArrowLength(3.5)
             .linkDirectionalArrowRelPos(1)
